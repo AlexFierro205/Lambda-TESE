db4a3fefb18df99f662b6177835101702c472aa3
AlexFierro205 <josealejandrovelazquezfierro@gmail.com>
2022-02-21 22:20:44 -0600

    update

--- Bienvenida.php
+++ Bienvenida.php
@@ -385,13 +385,6 @@
                 </div>
             </div>
         </div>
-        <div class="rights flex-row">
-            <h4 class="text-gray">
-                Copyright ©2019 All rights reserved | made by
-                <a href="www.youtube.com/c/dailytuition" target="_black">Daily Tuition <i class="fab fa-youtube"></i>
-                    Channel</a>
-            </h4>
-        </div>
         <div class="move-up">
             <span><i class="fas fa-arrow-circle-up fa-2x"></i></span>
         </div>
